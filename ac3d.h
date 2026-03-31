e6038aea7dbf38abcdacb151110a399e7531e7cd
Robert Reif <reif@earthlink.net>
2026-03-31 19:30:50 -0400

    update copyright date

--- ac3d.h
+++ ac3d.h
@@ -1,6 +1,6 @@
 /*
  * acclint - A tool that detects errors in AC3D files.
- * Copyright (C) 2020-2024 Robert Reif
+ * Copyright (C) 2020-2026 Robert Reif
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
